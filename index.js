bfad8328c0d6ca2d3a178ed756701d3d3a2ce5ff
Micah Banschick <micha.ban@gmail.com>
2021-03-05 23:43:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 20;
+let num4 = 8;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,20);
