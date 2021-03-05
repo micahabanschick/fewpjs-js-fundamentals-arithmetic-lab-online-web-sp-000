d79411919cd8f0ef69c92662250438abed90cc6a
Micah Banschick <micha.ban@gmail.com>
2021-03-05 23:41:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 // Write your code here
 let num1 = 2;
 let num2 = 31;
+let num3 = 20;
+le num4 = 8;
 
 const multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 10) + 1;
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3 % num4;
